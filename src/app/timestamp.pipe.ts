c53597566901c4972451e46bd8fc710eaf9f32d1
Round0000 <billybinche@gmail.com>
2022-08-24 16:20:32 +0200

    commit

--- src/app/timestamp.pipe.ts
+++ src/app/timestamp.pipe.ts
@@ -12,8 +12,8 @@ export class TimestampPipe implements PipeTransform {
     let format = 'hh:mm';
 
     // const YYYY = value.getFullYear().toString();
-    // const MM = (value.getMonth() + 1).toString().padStart(2, '0');
-    // const DD = value.getDate().toString().padStart(2, '0');
+    const MM = (value.getMonth() + 1).toString().padStart(2, '0');
+    const DD = value.getDate().toString().padStart(2, '0');
     const hh = value.getHours().toString().padStart(2, '0');
     const mm = value.getMinutes().toString().padStart(2, '0');
     // const ss = value.getSeconds().toString().padStart(2, '0');
@@ -23,6 +23,18 @@ export class TimestampPipe implements PipeTransform {
     format = format.replace('hh', hh);
     format = format.replace('mm', mm);
     // format = format.replace('ss', ss);
+
+    let prefix = '';
+    if (value.getDate() === new Date().getDate() - 1) {
+      prefix = 'Hier à ';
+    } else if (value.getDate() === new Date().getDate() - 2) {
+      prefix = 'Avant-hier à ';
+    } else if (new Date().getDate() - 2 > value.getDate()) {
+      prefix = DD + '/' + MM + ' à ';
+    }
+
+    format = prefix + format;
+
     return format;
   }
 }
